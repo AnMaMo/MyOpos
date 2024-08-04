fb77a2313cc06c178c902a534a7ac5de61cb13c5
Andre <andremartinezmo@gmail.com>
2024-08-04 12:07:31 +0200

    deploy

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,14 +1,25 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
+import { Link } from '@inertiajs/vue3';
 </script>
 
 <template>
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+
     <Head title="Tablero" />
 
     <AuthenticatedLayout>
 
-        
+        <Link :href="route('Tests')">
+        <div class="w-1/5 h-40 flex items-center justify-center text-white border-black-2">
+            <span class="material-symbols-outlined">
+                quiz
+            </span>
+        </div>
+        </Link>
+
 
     </AuthenticatedLayout>
 </template>
