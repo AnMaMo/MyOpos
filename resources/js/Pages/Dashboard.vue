0308d855fa3de592bf2f4fc8071811fef7efd250
Andre <andremartinezmo@gmail.com>
2024-08-04 15:33:54 +0200

    deploy

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -19,6 +19,7 @@ import { Link } from '@inertiajs/vue3';
             </span>
         </div>
         </Link> -->
+
         
 
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
