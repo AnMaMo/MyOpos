8d8b285fbbc40248b1f9facf1f1f040c8d5bdc8d
Andre <andremartinezmo@gmail.com>
2025-01-18 17:08:00 +0100

    Deploy

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -33,6 +33,7 @@ class AuthenticatedSessionController extends Controller
     {
         $request->authenticate();
 
+        
         $request->session()->regenerate();
 
         // Regenera el token CSRF explícitamente
